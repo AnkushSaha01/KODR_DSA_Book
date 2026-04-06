a691ad596ca57d19efeba51a353bbf7cf456b031
AnkushSaha01 <ankush05cse@gmail.com>
2026-04-07 02:27:37 +0530

    Q83

---
+++ week 2/Q83.js
@@ -0,0 +1,14 @@
+//Count Operations to Obtain Zero
+
+let num1=2, num2=3, count=0;
+
+while(num1 !== 0 && num2 !== 0){
+  if(num1 >= num2){
+    num1 -= num2;
+  }else{
+    num2 -= num1;
+  }
+  count++;
+}
+
+console.log(count);
