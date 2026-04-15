a7f8dd9644a9a3bbbed03baaa36ec00255478372
AnkushSaha01 <ankush05cse@gmail.com>
2026-04-16 02:09:31 +0530

    Q98

---
+++ Week 3/Q98.js
@@ -0,0 +1,14 @@
+//Search a 2D Matrix
+
+let matrix = [[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], target = 3
+
+for (let i = 0; i < matrix.length; i++) {
+  for (let j = 0; j < matrix[i].length; j++) {
+    if (matrix[i][j] === target) {
+      console.log(true)
+      return
+    }
+  }
+}
+
+console.log(false)
